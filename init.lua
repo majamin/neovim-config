b106faa8f66bb8e40f5cc5c2fdbb518a576bf6a7
Marian Minar <majamin@gmail.com>
2022-08-22 10:56:42 -0700

    actually add autocmd

--- init.lua
+++ init.lua
@@ -1,18 +1,19 @@
-require('majamin.packer_init')
-require('majamin.base')
-require('majamin.highlights')
-require('majamin.maps')
-require('majamin.plugins')
+require("majamin.packer_init")
+require("majamin.base")
+require("majamin.autocmd")
+require("majamin.highlights")
+require("majamin.maps")
+require("majamin.plugins")
 
 local has = function(x)
   return vim.fn.has(x) == 1
 end
-local is_mac = has "macunix"
-local is_win = has "win32"
+local is_mac = has("macunix")
+local is_win = has("win32")
 
 if is_mac then
-  require('majamin.macos')
+  require("majamin.macos")
 end
 if is_win then
-  require('majamin.windows')
+  require("majamin.windows")
 end
