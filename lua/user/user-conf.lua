b1f0d44d25f8bf214f06cbb7b4c163dde04e65ae
Marian Minar <majamin@gmail.com>
2022-08-30 18:28:03 -0700

    add vue and R stuff

--- lua/user/user-conf.lua
+++ lua/user/user-conf.lua
@@ -5,12 +5,14 @@ M.lang = "en_ca" -- Canadian English
 
 -- Listing LSP servers here will install and setup defaults
 -- Some further configuration may need to occur in `plugin/lspconfig.lua` (but most don't require this)
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 M.servers = {
   "sumneko_lua",
   "tsserver",
   "volar",
   "tailwindcss",
   "pyright",
+  "r_language_server",
   --"hls",
 }
 
