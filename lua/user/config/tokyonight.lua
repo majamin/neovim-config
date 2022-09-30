a8ba2aa5dc6a98e7f7a5667cbc67a22a655eeb17
Marian Minar <majamin@gmail.com>
2022-09-30 16:15:53 -0700

    Allow transparent background

--- lua/user/config/tokyonight.lua
+++ lua/user/config/tokyonight.lua
@@ -6,7 +6,7 @@ tokyo.setup({
   -- your configuration comes here
   -- or leave it empty to use the default settings
   style = "storm", -- The theme comes in three styles, `storm`, a darker variant `night` and `day`
-  transparent = false, -- Enable this to disable setting the background color
+  transparent = true, -- Enable this to disable setting the background color
   terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
   styles = {
     -- Style to be applied to different syntax groups
