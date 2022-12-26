16ae6991604c94f86ddf52314f3990dd66973886
Marian Minar <majamin@gmail.com>
2022-12-25 22:14:40 -0800

    fixed indent color line not showing

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,6 +1,6 @@
 local M = {
   "jesseleite/nvim-noirbuddy",
-  dependencies = { "tjdevries/colorbuddy.nvim", branch = "dev" }
+  dependencies = { "tjdevries/colorbuddy.nvim", branch = "dev" },
 }
 
 function M.config()
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,9 +2,12 @@ return {
   { "jpalardy/vim-slime" },
   { "tpope/vim-fugitive" },
   { "norcalli/nvim-colorizer.lua" },
-  { "lukas-reineke/indent-blankline.nvim" },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    dependencies = { "jesseleite/nvim-noirbuddy" }, -- fixes indent line colors
+  },
   { "folke/zen-mode.nvim" },
-  { "windwp/nvim-ts-autotag", ft = { "html", "tsx", "vue", "svelte", "php", "res" }, },
+  { "windwp/nvim-ts-autotag", ft = { "html", "tsx", "vue", "svelte", "php", "res" } },
   { "windwp/nvim-autopairs" },
   { "numToStr/Comment.nvim" },
   { "tpope/vim-surround" },
