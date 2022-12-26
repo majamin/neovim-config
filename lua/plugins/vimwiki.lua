ff438c8b9ba1e233831881120b06c71ec49042ee
Marian Minar <majamin@gmail.com>
2022-12-25 22:15:04 -0800

    fix vimwiki home wiki

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -2,13 +2,11 @@ local M = {
   "vimwiki/vimwiki",
 }
 
-function M.config()
+function M.init()
+  local opts = { silent = true }
   vim.g.vimwiki_global_ext  = 0 -- Don't vimwiki every goddamn md file, please
   vim.g.vimwiki_url_maxsave = 0 -- Don't shorten URL so you can plumb links
   vim.g.vimwiki_list = { { path = os.getenv("ONEDRIVE") .. "/Projects/notes" } }
-end
-
-function M.init()
   vim.keymap.set("n", "<S-F19>", "<Plug>VimwikiNextLink", opts, { desc = "Unnecessary Vimwiki binding" })
   vim.keymap.set("n", "<S-F18>", "<Plug>VimwikiPrevLink", opts, { desc = "Unnecessary Vimwiki binding" })
   vim.keymap.set("n", ",w", "<Plug>VimwikiIndex", opts, { desc = "Open default Vimwiki" })
