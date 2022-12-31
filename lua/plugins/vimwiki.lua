228cca7e35e7b83f3d11e44f13d4c00c6d122be1
Marian Minar <majamin@gmail.com>
2022-12-31 11:21:21 -0800

    lazy load vimwiki

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -2,17 +2,15 @@ local M = {
   "vimwiki/vimwiki",
 }
 
-function M.init()
-  local opts = { silent = true }
-  vim.g.vimwiki_global_ext  = 0 -- Don't vimwiki every goddamn md file, please
-  vim.g.vimwiki_url_maxsave = 0 -- Don't shorten URL so you can plumb links
-  vim.g.vimwiki_list = { { path = os.getenv("ONEDRIVE") .. "/Projects/notes" } }
-  vim.keymap.set("n", "<S-F19>", "<Plug>VimwikiNextLink", opts, { desc = "Unnecessary Vimwiki binding" })
-  vim.keymap.set("n", "<S-F18>", "<Plug>VimwikiPrevLink", opts, { desc = "Unnecessary Vimwiki binding" })
-  vim.keymap.set("n", ",w", "<Plug>VimwikiIndex", opts, { desc = "Open default Vimwiki" })
-  vim.keymap.set("n", ",wi", "<Plug>VimwikiDiaryIndex", opts, { desc = "Open default Vimwiki" })
-  vim.keymap.set("n", ",ws", "<Plug>VimwikiUISelect", opts, { desc = "Open Vimwiki UI Select" })
-  vim.keymap.set("n", ",wt", "<Plug>VimwikiTabIndex", opts, { desc = "Open Vimwiki Tab Index" })
-end
+local opts = { silent = true }
+
+M.keys = {
+  { "<S-F19>", "<Plug>VimwikiNextLink", opts, { desc = "Unnecessary Vimwiki binding" }},
+  { "<S-F18>", "<Plug>VimwikiPrevLink", opts, { desc = "Unnecessary Vimwiki binding" }},
+  { ",w", "<Plug>VimwikiIndex", opts, { desc = "Open default Vimwiki" }},
+  { ",wi", "<Plug>VimwikiDiaryIndex", opts, { desc = "Open default Vimwiki" }},
+  { ",ws", "<Plug>VimwikiUISelect", opts, { desc = "Open Vimwiki UI Select" }},
+  { ",wt", "<Plug>VimwikiTabIndex", opts, { desc = "Open Vimwiki Tab Index" }},
+}
 
 return M
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -4,6 +4,9 @@ local g = vim.g
 local user = require("user")
 
 g.mapleader = user.mapleader
+g.vimwiki_global_ext  = 0 -- Don't vimwiki every goddamn md file, please
+g.vimwiki_url_maxsave = 0 -- Don't shorten URL so you can plumb links
+g.vimwiki_list = { { path = os.getenv("ONEDRIVE") .. "/Projects/notes" } }
 o.backup = false -- creates a backup file
 o.breakindent = true
 o.background = user.background
