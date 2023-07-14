0841398cf42b93766de3ba5668f75aba97ced70d
Marian Minar <majamin@gmail.com>
2023-07-14 13:35:09 -0700

    update LSP

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -20,22 +20,52 @@ M.servers = {
   },
 }
 
-M.on_attach = function(_, bufnr)
-  nmap(lsp_leader .. 'r', vim.lsp.buf.rename, bufnr, '[R]e[n]ame')
-  nmap(lsp_leader .. 'a', vim.lsp.buf.code_action, bufnr, '[C]ode [A]ction')
-  nmap('gd', vim.lsp.buf.definition, bufnr, '[G]oto [D]efinition')
-  nmap('gr', require('telescope.builtin').lsp_references, bufnr, '[G]oto [R]eferences')
-  nmap('gI', vim.lsp.buf.implementation, bufnr, '[G]oto [I]mplementation')
-  nmap(lsp_leader .. 'D', vim.lsp.buf.type_definition, bufnr, 'Type [D]efinition')
-  nmap(lsp_leader .. 'ds', require('telescope.builtin').lsp_document_symbols, bufnr, '[D]ocument [S]ymbols')
-  nmap(lsp_leader .. 'ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, bufnr, '[W]orkspace [S]ymbols')
-  nmap('K', vim.lsp.buf.hover, bufnr, 'Hover Documentation')
-  nmap('gD', vim.lsp.buf.declaration, bufnr, '[G]oto [D]eclaration')
+M.on_attach = function(client, bufnr)
+  local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+  local format_code
+  if client.supports_method("textDocument/formatting") then
+    format_code = "<cmd>lua vim.lsp.buf.format()<CR>"
+  elseif client.supports_method("textDocument/rangeFormatting") then
+    format_code = "<cmd>lua vim.lsp.buf.range_formatting()<CR>"
+  elseif vim.tbl_contains({ "go", "rust" }, filetype) then
+    format_code = "<cmd>lua vim.lsp.buf.formatting_sync()<CR>"
+  else
+    format_code = "<cmd>Format<CR>"
+  end
+
+  nmap(lsp_leader .. "r", vim.lsp.buf.rename, bufnr, "[R]e[n]ame")
+  nmap(lsp_leader .. "a", vim.lsp.buf.code_action, bufnr, "[C]ode [A]ction")
+  nmap("gd", vim.lsp.buf.definition, bufnr, "[G]oto [D]efinition")
+  nmap(
+    "gr",
+    require("telescope.builtin").lsp_references,
+    bufnr,
+    "[G]oto [R]eferences"
+  )
+  nmap("gI", vim.lsp.buf.implementation, bufnr, "[G]oto [I]mplementation")
+  nmap(
+    lsp_leader .. "D",
+    vim.lsp.buf.type_definition,
+    bufnr,
+    "Type [D]efinition"
+  )
+  nmap(
+    lsp_leader .. "ds",
+    require("telescope.builtin").lsp_document_symbols,
+    bufnr,
+    "[D]ocument [S]ymbols"
+  )
+  nmap(
+    lsp_leader .. "ws",
+    require("telescope.builtin").lsp_dynamic_workspace_symbols,
+    bufnr,
+    "[W]orkspace [S]ymbols"
+  )
+  nmap("K", vim.lsp.buf.hover, bufnr, "Hover Documentation")
+  nmap("gD", vim.lsp.buf.declaration, bufnr, "[G]oto [D]eclaration")
 
   -- Create a command `:Format` local to the LSP buffer
-  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    vim.lsp.buf.format()
-  end, { desc = 'Format current buffer with LSP' })
+  nmap(lsp_leader .. "f", format_code, bufnr, "[F]ormat buffer")
 end
 
 return M
