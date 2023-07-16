46f5a561a776ce95a6e74dad77c1ca5266224c7c
Marian Minar <majamin@gmail.com>
2023-07-16 13:05:50 -0700

    adding some plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,19 @@
 return {
   "tpope/vim-fugitive",
   "tpope/vim-sleuth",
+  "tpope/vim-surround",
+  { "windwp/nvim-ts-autotag", config = true },
+  { "windwp/nvim-autopairs", config = true },
   { "numToStr/Comment.nvim", opts = {} },
+  {
+    "mbbill/undotree",
+    keys = {
+      {
+        "U",
+        "<cmd>UndotreeToggle|UndotreeFocus<CR>",
+        desc = "Open Undotree",
+        { silent = true },
+      },
+    },
+  },
 }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -5,15 +5,12 @@ local M = {
 
 M.config = function()
   local starter = require("mini.starter")
-  local mini = require("mini.sessions")
-  mini.setup()
   starter.setup({
     evaluate_single = true,
     items = {
       starter.sections.builtin_actions(),
       starter.sections.recent_files(6, true),
       starter.sections.recent_files(6, false),
-      starter.sections.sessions(3, true),
     },
     content_hooks = {
       starter.gen_hook.adding_bullet(),
