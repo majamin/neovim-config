d567101c2b15f3209cbfe7207839675f1ce01f02
Marian Minar <majamin@gmail.com>
2023-08-20 09:16:53 -0700

    todo comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,9 +4,10 @@ return {
   { "famiu/bufdelete.nvim",     cmd = { "Bdelete" } },
   { "tpope/vim-sleuth",         event = "VeryLazy" },
   { "tpope/vim-surround",       event = "VeryLazy" },
-  { "windwp/nvim-ts-autotag",   config = true,      event = "VeryLazy" },
-  { "windwp/nvim-autopairs",    config = true,      event = "VeryLazy" },
-  { "numToStr/Comment.nvim",    config = true,      event = "VeryLazy" },
-  { "lewis6991/gitsigns.nvim",  config = true,      event = "VeryLazy" },
-  { 'echasnovski/mini.tabline', config = true,      version = '*' },
+  { "windwp/nvim-ts-autotag",   config = true,            event = "VeryLazy" },
+  { "windwp/nvim-autopairs",    config = true,            event = "VeryLazy" },
+  { "numToStr/Comment.nvim",    config = true,            event = "VeryLazy" },
+  { "lewis6991/gitsigns.nvim",  config = true,            event = "VeryLazy" },
+  { "echasnovski/mini.tabline", config = true,            version = "*" },
+  { "folke/todo-comments.nvim", opts = { signs = false }, dependencies = { "nvim-lua/plenary.nvim" }, }
 }
