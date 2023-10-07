e63d6c0ae969afdfb05bf2c69c81a7f506cb1304
Marian Minar <majamin@gmail.com>
2023-10-07 10:01:46 -0700

    organize maps, add gitignore

--- lua/user/keys.lua
+++ lua/user/keys.lua
@@ -3,15 +3,20 @@
 local M = {}
 local opts = { silent = true }
 
-vim.keymap.set({ "n" }, "x", '"_x')
-vim.keymap.set({ "n" }, "<ESC>", ":noh<CR>", opts)
-vim.keymap.set({ "v" }, "<", "<gv", opts)
-vim.keymap.set({ "v" }, ">", ">gv", opts)
+-- Improvements to defaults
+vim.keymap.set({ "n" }, "x", '"_x') -- send single chars to blackhole
+vim.keymap.set({ "n" }, "<ESC>", ":noh<CR>", opts) -- turn off search hi
+vim.keymap.set({ "v" }, "<", "<gv", opts) -- keep selection after outdent
+vim.keymap.set({ "v" }, ">", ">gv", opts) -- keep selection after indent
+
+-- Nice-to-haves
 vim.keymap.set({ "n" }, "<BS>", "<C-^>", opts)
 vim.keymap.set({ "n" }, "<TAB>", ":bn<CR>", opts)
 vim.keymap.set({ "n" }, "<S-TAB>", ":bp<CR>", opts)
 vim.keymap.set({ "n" }, "<C-w>b", ":Bdelete<CR>", opts)
 vim.keymap.set({ "n" }, "<F3>", ":setlocal spell!<CR>", opts)
+
+-- Find images, mark them with 'm', paste paths to file
 vim.keymap.set(
   { "n" },
   "<leader>i",
@@ -19,13 +24,13 @@ vim.keymap.set(
   opts
 )
 
--- Yank and paste to tmux clipboard
 vim.keymap.set(
   { "n", "v" },
   "<leader><leader>y",
   'y<cr>:call system("tmux load-buffer -", @0)<cr>gv',
   opts -- { desc = "Yank to the tmux clipboard" }
 )
+
 vim.keymap.set(
   { "n", "v" },
   "<leader><leader>p",
@@ -39,6 +44,17 @@ vim.keymap.set(
   ':w! | !compiler "%:p"<CR>',
   opts -- { desc = "Run compiler on current file" }
 )
+
+vim.keymap.set(
+  { "n" },
+  "<F2>",
+  function()
+    vim.cmd(':wa|mksession!<CR>')
+    vim.print("Session and files saved")
+  end,
+  opts -- { desc = "Make session and save files" }
+)
+
 
 -- OTHER USEFUL KEYMAPS
 
