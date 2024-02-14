a14946a2e716319c38ef446a7ac9d5cff203177e
Marian Minar <majamin@gmail.com>
2024-02-14 07:44:24 -0800

    update packages, reformat init plugs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,14 +1,23 @@
 return {
-  { "github/copilot.vim" },
-  { "tpope/vim-sleuth",        event = "VeryLazy" },
-  { "tpope/vim-surround",      event = "VeryLazy" },
-  { "famiu/bufdelete.nvim",    cmd = { "Bdelete" },                                           event = "VeryLazy" },
-  { "windwp/nvim-autopairs",   config = true,                                                 event = "VeryLazy" },
-  { "numToStr/Comment.nvim",   config = true,                                                 event = "VeryLazy" },
-  { "lewis6991/gitsigns.nvim", config = true,                                                 event = "VeryLazy" },
-  { "jpalardy/vim-slime",      config = function() vim.cmd("let g:slime_target = 'tmux'") end },
+  { "tpope/vim-sleuth",                             event = "VeryLazy" },
+  { "tpope/vim-surround",                           event = "VeryLazy" },
+  { "famiu/bufdelete.nvim",    cmd = { "Bdelete" }, event = "VeryLazy" },
+  { "windwp/nvim-autopairs",   config = true,       event = "VeryLazy" },
+  { "numToStr/Comment.nvim",   config = true,       event = "VeryLazy" },
+  { "lewis6991/gitsigns.nvim", config = true,       event = "VeryLazy" },
   { "norcalli/nvim-colorizer.lua", config = true },
-  { dir = "/home/marian/Projects/flashbang.nvim" },
+  { "jpalardy/vim-slime",      config = function()
+      vim.cmd("let g:slime_target = 'tmux'")
+    end
+  },
+  { "github/copilot.vim", config = function()
+      vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+        expr = true,
+        replace_keycodes = false
+      })
+      vim.g.copilot_no_tab_map = true
+    end
+  },
   {
     "echasnovski/mini.tabline",
     config = true,
