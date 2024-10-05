210abfc675281c762ed85805d6b2d2243eedcdfa
Marian Minar <majamin@gmail.com>
2024-10-04 17:04:08 -0700

    add colorscheme logic

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,25 @@
+local get_colorscheme = require("user.funs").get_colorscheme
+local set_colorscheme = require("user.funs").set_colorscheme
 local opts = require("user.opts")
+
+-- @param name: string
+-- @param variants: table
+-- @return boolean
+local check_variant = function(name,variants)
+  for _,v in pairs(variants) do
+    if name == v then
+      return true
+    end
+  end
+  return false
+end
+
 return {
   -- Tokyonight theme
   {
     "folke/tokyonight.nvim", -- https://github.com/folke/tokyonight.nvim/releases/latest
 
     config = function()
-      local colorscheme = require("user.opts").colorscheme
       local variants = {
         "tokyonight",
         "tokyonight-day",
@@ -14,15 +28,6 @@ return {
         "tokyonight-storm",
       }
       -- check if the colorscheme is in the list of variants
-      local variant
-      for _, v in pairs(variants) do
-        if not colorscheme == v then
-          return
-        else
-          variant = v
-        end
-      end
-
       require("tokyonight").setup({
         transparent = opts.bg_is_transparent,
         sidebars = { "qf", "help", "harpoon", "TelescopePrompt" }, -- filetype windows with darker background
@@ -30,19 +35,44 @@ return {
         lualine_bold = true, -- When `true`, section headers in the lualine theme will be bold
       })
 
-      -- set the scheme
-      vim.cmd("colorscheme " .. variant)
+      local colorscheme = get_colorscheme()
+      if not check_variant(colorscheme, variants) then
+        return
+      else
+        set_colorscheme(colorscheme)
+      end
+
     end,
   },
   -- GitHub theme
   {
     "projekt0n/github-nvim-theme",
-    lazy = false,
-    priority = 1000,
     config = function()
+      local variants = {
+        "github_dark",
+        "github_dark_colorblind",
+        "github_dark_default",
+        "github_dark_dimmed",
+        "github_dark_high_contrast",
+        "github_dark_tritanopia",
+        "github_light",
+        "github_light_colorblind",
+        "github_light_default",
+        "github_light_high_contrast",
+        "github_light_tritanopia",
+      }
+
       require("github-theme").setup({
         options = { transparent = opts.bg_is_transparent },
       })
+
+      local colorscheme = get_colorscheme()
+      if not check_variant(colorscheme, variants) then
+        return
+      else
+        set_colorscheme(colorscheme)
+      end
+
     end,
   },
 }
