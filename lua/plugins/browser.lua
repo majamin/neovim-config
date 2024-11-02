f59f192a632ab2e7f6c851e84749d7392c025049
Marian Minar <majamin@gmail.com>
2024-11-02 09:51:24 -0700

    add colors to oil

--- lua/plugins/browser.lua
+++ lua/plugins/browser.lua
@@ -4,27 +4,43 @@ local M = {
 }
 
 M.config = function()
+  -- https://github.com/stevearc/oil.nvim/pull/240
+  local permission_hlgroups = {
+    ["-"] = "NonText",
+    ["r"] = "DiagnosticSignWarn",
+    ["w"] = "DiagnosticSignError",
+    ["x"] = "DiagnosticSignOk",
+  }
+
   require("oil").setup({
-    default_file_explorer = true,
-    skip_confirm_for_simple_edits = true,
     columns = {
-      "permissions",
-      "size",
-      "mtime",
-    },
-    float = {
-      -- Padding around the floating window
-      max_width = 60,
-      border = "rounded",
-      max_height = 30,
-      win_options = {
-        winblend = 10,
+      {
+        "permissions",
+        highlight = function(permission_str)
+          local hls = {}
+          for i = 1, #permission_str do
+            local char = permission_str:sub(i, i)
+            table.insert(hls, { permission_hlgroups[char], i - 1, i })
+          end
+          return hls
+        end,
       },
-      -- This is the config that will be passed to nvim_open_win.
-      -- Change values here to customize the layout
-      override = function(conf)
-        return conf
-      end,
+      { "size", highlight = "Special" },
+      { "mtime", highlight = "Number" },
+      {
+        "icon",
+        default_file = "",
+        directory = "",
+        add_padding = false,
+      },
+    },
+    skip_confirm_for_simple_edits = true,
+    win_options = {
+      number = false,
+      relativenumber = false,
+      signcolumn = "no",
+      foldcolumn = "0",
+      statuscolumn = "",
     },
     keymaps = {
       ["<ESC>"] = "actions.close",
