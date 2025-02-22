b216ed1eb73e241854a0088aaf0a0d689dc38984
Marian Minar <majamin@gmail.com>
2025-02-22 15:25:48 -0800

    update packages and blink

--- init.lua
+++ init.lua
@@ -18,56 +18,57 @@ g.mapleader = ";"
 g.maplocalleader = ";"
 
 -- stylua: ignore start
-g.have_nerd_font = true         -- set to false if you don't have a nerd font
-o.backspace = ""                -- influences the working of <BS>, etc. see `:h 'backspace'`
-o.backup = false                -- <true|false> make a backup before overwriting a file?
-o.breakindent = true            -- <true|false> Will every wrapped line continue visually indented?
-o.clipboard = "unnamedplus"     -- <string> set clipboard mode - see `:h 'clipboard'`
-o.cmdheight = 1                 -- <int> space for displaying messages/commands?
+g.have_nerd_font = true                                       -- set to false if you don't have a nerd font
+o.backspace = ""                                              -- influences the working of <BS>, etc. see `:h 'backspace'`
+o.backup = false                                              -- <true|false> make a backup before overwriting a file?
+o.breakindent = true                                          -- <true|false> Will every wrapped line continue visually indented?
+o.clipboard = "unnamedplus"                                   -- <string> set clipboard mode - see `:h 'clipboard'`
+o.cmdheight = 1                                               -- <int> space for displaying messages/commands?
 o.completeopt = { "menu", "menuone", "noselect", "noinsert" } -- options for Insert mode completion
-o.conceallevel = 3              -- {0,1,2,3} Determine how text with "conceal" is shown
-o.cursorline = true             -- <true|false> highlight the current line?
-o.expandtab = true              -- <true | false> convert tabs to spaces?
-o.fileencoding = "utf-8"        -- <fileencodings> file-content encoding for the current buffer
-o.fillchars = { fold = "·" }    -- see `:h 'fillchars'`
-o.foldcolumn = "0"              -- <string> 'auto' or number of columns to use for the fold column
-o.foldenable = false            -- <true|false> all folds closed on buffer open? (zi to toggle)
-o.foldtext = "v:lua.myfolds()"  -- see lua/user/funs.lua
-o.history = 500                 -- <number> a history of ":" and search commands (max 10000)
-o.hlsearch = true               -- <boolean> highlight all matches on previous search pattern
-o.ignorecase = false            -- <boolean> ignore case in search patterns
-o.laststatus = 3                -- {0,1,2,3} should the last window will have a status line? See `:h laststatus`
-o.lazyredraw = true             -- <boolean> redraw screen while running macros? (true = faster)
-o.list = true                   -- <boolean> show some invisible characters (tabs, eol, etc.)
-o.mouse = ""                    -- <string> enable mouse support in which situations? See `:h 'mouse'`
-o.number = true                 -- <boolean> print the line number in front of each line?
-o.path = ".,**"                 -- <string> search paths. See `:h 'path'` and `:h starstar-wildcard`
-o.relativenumber = true         -- <boolean> show relative line numbers?
-o.rtp:prepend(lazypath)         -- add lazy to rtp
-o.scrolloff = 3                 -- <number> minimum number of screen lines to keep above and below the cursor
-o.shiftwidth = 2                -- <number> the number of spaces inserted for each indentation
-o.shortmess = o.shortmess + "c" -- prevent "pattern not found" messages
-o.showmode = false              -- <boolean> show the mode? (redundant if mode is built into the statusline)
-o.sidescrolloff = 5             -- <number> the minimal number of columns to scroll horizontally
-o.signcolumn = "yes"            -- <string> in {auto, no, yes, 1-9} - "yes" prevents gutter opening and closing spastically
-o.smartcase = true              -- <boolean> override the 'ignorecase' option if the search pattern contains upper case characters?
-o.smartindent = true            -- <boolean> make indenting smarter again?
-o.spelllang = "en_ca"           -- <string> language(s) to use for spell checking
-o.splitbelow = true             -- <boolean> force all horizontal splits to go below current window?
-o.splitright = true             -- <boolean> force all vertical splits to go to the right of current window?
-o.swapfile = true               -- <boolean> enable/disable swap file creation?
-o.tabstop = 2                   -- <number> default: 8 - how many columns does a tab counts for?
-o.termguicolors = true          -- <boolean> enable 24-bit RGB color in the TUI?
-o.timeoutlen = 400              -- <number> time to wait for a mapped sequence to complete (in milliseconds)
-o.ttimeoutlen = 0               -- <number> time in milliseconds to wait for a key code sequence to complete
-o.undofile = true               -- <boolean> enable/disable undo file creation?
-o.updatetime = 1000             -- <number> if nothing is typed after this many milliseconds the swap file will be written to disk
-o.wildignorecase = true         -- <boolean> ignore setcase when completing file names and directories?
-o.wildmode = "full"             -- <string> how to complete file names and directories
-o.wrap = false                  -- <boolean> wrap lines at the screen edge?
+o.conceallevel = 3                                            -- {0,1,2,3} Determine how text with "conceal" is shown
+o.cursorline = true                                           -- <true|false> highlight the current line?
+o.expandtab = true                                            -- <true | false> convert tabs to spaces?
+o.fileencoding = "utf-8"                                      -- <fileencodings> file-content encoding for the current buffer
+o.fillchars = { fold = "·" }                                  -- see `:h 'fillchars'`
+o.foldcolumn = "0"                                            -- <string> 'auto' or number of columns to use for the fold column
+o.foldenable = false                                          -- <true|false> all folds closed on buffer open? (zi to toggle)
+o.foldtext = "v:lua.myfolds()"                                -- see lua/user/funs.lua
+o.history = 500                                               -- <number> a history of ":" and search commands (max 10000)
+o.hlsearch = true                                             -- <boolean> highlight all matches on previous search pattern
+o.ignorecase = false                                          -- <boolean> ignore case in search patterns
+o.laststatus = 3                                              -- {0,1,2,3} should the last window will have a status line? See `:h laststatus`
+o.lazyredraw = true                                           -- <boolean> redraw screen while running macros? (true = faster)
+o.list = true                                                 -- <boolean> show some invisible characters (tabs, eol, etc.)
+o.mouse = ""                                                  -- <string> enable mouse support in which situations? See `:h 'mouse'`
+o.number = true                                               -- <boolean> print the line number in front of each line?
+o.path = ".,**"                                               -- <string> search paths. See `:h 'path'` and `:h starstar-wildcard`
+o.relativenumber = true                                       -- <boolean> show relative line numbers?
+o.rtp:prepend(lazypath)                                       -- add lazy to rtp
+o.scrolloff = 3                                               -- <number> minimum number of screen lines to keep above and below the cursor
+o.shiftwidth = 2                                              -- <number> the number of spaces inserted for each indentation
+o.shortmess = o.shortmess + "c"                               -- prevent "pattern not found" messages
+o.showmode = false                                            -- <boolean> show the mode? (redundant if mode is built into the statusline)
+o.sidescrolloff = 5                                           -- <number> the minimal number of columns to scroll horizontally
+o.signcolumn =
+"yes"                                                         -- <string> in {auto, no, yes, 1-9} - "yes" prevents gutter opening and closing spastically
+o.smartcase = true                                            -- <boolean> override the 'ignorecase' option if the search pattern contains upper case characters?
+o.smartindent = true                                          -- <boolean> make indenting smarter again?
+o.spelllang = "en_ca"                                         -- <string> language(s) to use for spell checking
+o.splitbelow = true                                           -- <boolean> force all horizontal splits to go below current window?
+o.splitright = true                                           -- <boolean> force all vertical splits to go to the right of current window?
+o.swapfile = true                                             -- <boolean> enable/disable swap file creation?
+o.tabstop = 2                                                 -- <number> default: 8 - how many columns does a tab counts for?
+o.termguicolors = true                                        -- <boolean> enable 24-bit RGB color in the TUI?
+o.timeoutlen = 400                                            -- <number> time to wait for a mapped sequence to complete (in milliseconds)
+o.ttimeoutlen = 0                                             -- <number> time in milliseconds to wait for a key code sequence to complete
+o.undofile = true                                             -- <boolean> enable/disable undo file creation?
+o.updatetime = 1000                                           -- <number> if nothing is typed after this many milliseconds the swap file will be written to disk
+o.wildignorecase = true                                       -- <boolean> ignore setcase when completing file names and directories?
+o.wildmode = "full"                                           -- <string> how to complete file names and directories
+o.wrap = false                                                -- <boolean> wrap lines at the screen edge?
 wo.foldexpr =
-"nvim_treesitter#foldexpr()"    -- foldexpr for treesitter. See `:h fold-expr` and `:h nvim_treesitter#foldexpr()`
-wo.foldmethod = "expr"          -- <string> foldmethod to use. See `:h foldmethod`
+"nvim_treesitter#foldexpr()"                                  -- foldexpr for treesitter. See `:h fold-expr` and `:h nvim_treesitter#foldexpr()`
+wo.foldmethod = "expr"                                        -- <string> foldmethod to use. See `:h foldmethod`
 -- stylua: ignore end
 
 local non_plugin_maps = {
@@ -144,10 +145,10 @@ end
 
 require("lazy").setup({
   spec = {
-    "tpope/vim-sleuth", --- https://github.com/tpope/vim-sleuth
+    "tpope/vim-sleuth",   --- https://github.com/tpope/vim-sleuth
     "tpope/vim-fugitive", --- https://github.com/tpope/vim-fugitive
     "tpope/vim-surround", --- https://github.com/tpope/vim-surround
-    { --- https://github.com/folke/tokyonight.nvim
+    {                     --- https://github.com/folke/tokyonight.nvim
       "folke/tokyonight.nvim",
       priority = 1000,
       config = function()
@@ -218,7 +219,9 @@ require("lazy").setup({
         keymap = {
           preset = "super-tab",
           ["<C-n>"] = { "show", "select_next", "fallback" },
-          cmdline = {
+        },
+        cmdline = {
+          keymap = {
             preset = "super-tab",
             ["<Up>"] = { "fallback" },
             ["<Down>"] = { "fallback" },
@@ -259,21 +262,21 @@ require("lazy").setup({
             local wk = require("which-key")
             wk.add({
               -- stylua: ignore start
-              { "<leader>l",  group = "LSP", icon = "" },
-              { "<leader>lr", vim.lsp.buf.rename,      desc = "LSP: Rename" },
+              { "<leader>l", group = "LSP", icon = "" },
+              { "<leader>lr", vim.lsp.buf.rename, desc = "LSP: Rename" },
               { "<leader>la", vim.lsp.buf.code_action, desc = "LSP: Code Action" },
-              { "K",          vim.lsp.buf.hover,       desc = "LSP: Hover Documentation" },
-              { "gd",         vim.lsp.buf.definition,  desc = "LSP: Goto Definition" },
-              { "gD",         vim.lsp.buf.declaration, desc = "LSP: Goto Declaration" },
+              { "K", vim.lsp.buf.hover, desc = "LSP: Hover Documentation" },
+              { "gd", vim.lsp.buf.definition, desc = "LSP: Goto Definition" },
+              { "gD", vim.lsp.buf.declaration, desc = "LSP: Goto Declaration" },
               -- stylua: ignore end
             })
 
             local client = vim.lsp.get_client_by_id(event.data.client_id)
 
             if
-              client
-              and client.server_capabilities.inlayHintProvider
-              and vim.lsp.inlay_hint
+                client
+                and client.server_capabilities.inlayHintProvider
+                and vim.lsp.inlay_hint
             then
               vim.lsp.inlay_hint.enable()
             end
@@ -284,7 +287,7 @@ require("lazy").setup({
           -- passing config.capabilities to blink.cmp merges with the capabilities in your
           -- `opts[server].capabilities, if you've defined it
           config.capabilities =
-            require("blink.cmp").get_lsp_capabilities(config.capabilities)
+              require("blink.cmp").get_lsp_capabilities(config.capabilities)
           lspconfig[server].setup(config)
         end
       end,
