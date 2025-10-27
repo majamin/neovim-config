ad0819508b58d40979b5d6e4357492b812a035f3
Marian Minar <majamin@gmail.com>
2025-10-27 13:49:26 -0700

    removing unused functions softly, add wrap to qf buffers

--- lua/auto.lua
+++ lua/auto.lua
@@ -1,38 +1,38 @@
-vim.api.nvim_create_user_command("AdocBuildALL", function()
-  local cmd =
-    [[tmux new-window 'fd "\\.adoc$" ./src | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk"' ]]
-  vim.fn.system(cmd)
-end, {})
-
-vim.api.nvim_create_user_command("AdocBuildHTML", function()
-  local file = vim.fn.expand("%:p") -- full path to current file
-  local cmd = string.format(
-    [[tmux new-window 'echo %s | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk single-html FILE=%s"' ]],
-    vim.fn.shellescape(file),
-    vim.fn.shellescape(file)
-  )
-  vim.fn.system(cmd)
-end, {})
-
-vim.api.nvim_create_user_command("AdocBuildREVEAL", function()
-  local file = vim.fn.expand("%:p") -- full path to current file
-  local cmd = string.format(
-    [[tmux new-window 'echo %s | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk single-reveal FILE=%s"' ]],
-    vim.fn.shellescape(file),
-    vim.fn.shellescape(file)
-  )
-  vim.fn.system(cmd)
-end, {})
-
-vim.api.nvim_create_user_command("AdocBuildPDF", function()
-  local file = vim.fn.expand("%:p") -- full path to current file
-  local cmd = string.format(
-    [[tmux new-window 'echo %s | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk single-pdf FILE=%s"' ]],
-    vim.fn.shellescape(file),
-    vim.fn.shellescape(file)
-  )
-  vim.fn.system(cmd)
-end, {})
+-- vim.api.nvim_create_user_command("AdocBuildALL", function()
+--   local cmd =
+--     [[tmux new-window 'fd "\\.adoc$" ./src | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk"' ]]
+--   vim.fn.system(cmd)
+-- end, {})
+--
+-- vim.api.nvim_create_user_command("AdocBuildHTML", function()
+--   local file = vim.fn.expand("%:p") -- full path to current file
+--   local cmd = string.format(
+--     [[tmux new-window 'echo %s | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk single-html FILE=%s"' ]],
+--     vim.fn.shellescape(file),
+--     vim.fn.shellescape(file)
+--   )
+--   vim.fn.system(cmd)
+-- end, {})
+--
+-- vim.api.nvim_create_user_command("AdocBuildREVEAL", function()
+--   local file = vim.fn.expand("%:p") -- full path to current file
+--   local cmd = string.format(
+--     [[tmux new-window 'echo %s | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk single-reveal FILE=%s"' ]],
+--     vim.fn.shellescape(file),
+--     vim.fn.shellescape(file)
+--   )
+--   vim.fn.system(cmd)
+-- end, {})
+--
+-- vim.api.nvim_create_user_command("AdocBuildPDF", function()
+--   local file = vim.fn.expand("%:p") -- full path to current file
+--   local cmd = string.format(
+--     [[tmux new-window 'echo %s | entr -c sh -c "make -f ~/.local/src/simple-asciidoctor-template/simple.mk single-pdf FILE=%s"' ]],
+--     vim.fn.shellescape(file),
+--     vim.fn.shellescape(file)
+--   )
+--   vim.fn.system(cmd)
+-- end, {})
 
 -- Set `>>` to `%>%` in R and Rmd files
 vim.api.nvim_create_autocmd("FileType", {
@@ -41,5 +41,12 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.schedule(function()
       vim.keymap.set("i", ">>", "%>%", { buffer = true })
     end)
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "qf" },
+  callback = function()
+    vim.opt_local.wrap = true
   end,
 })
