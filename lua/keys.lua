21ecf0398498f66db4034339325ea02ed54e1cb4
Marian Minar <majamin@gmail.com>
2025-11-23 10:07:59 -0800

    improve diagnostic messages

--- lua/keys.lua
+++ lua/keys.lua
@@ -1,7 +1,5 @@
 local M = {}
 
-local have_window_by_filetype = require("funs").have_window_by_filetype
-
 M.non_plugin_maps = {
   -- stylua: ignore start
   { "<Esc>", "<cmd>nohlsearch<CR>" },
@@ -10,16 +8,11 @@ M.non_plugin_maps = {
   { "<F4>", ":setlocal spell!<CR>", desc = "Toggle spellcheck" },
   { "-", "<cmd>Oil<CR>", desc = "Browse files with Oil" },
   { "<C-w>b", "<cmd>bdelete<CR>", desc = "Close buffer" },
-  { "<leader>q", function()
-      if have_window_by_filetype("qf") then
-        vim.cmd("lclose")
-      else
-        vim.diagnostic.setloclist({ open = true })
-      end
-    end
-    , desc = "Toggle diagnostics", icon = "", },
+  { "<leader>q", function() require("snacks").picker.diagnostics() end, desc = "Show diagnostics", icon = "", },
+  { "]d", vim.diagnostic.goto_next, desc = "Next diagnostic" },
+  { "[d", vim.diagnostic.goto_prev, desc = "Previous diagnostic" },
   { "<leader>e", "<cmd>!xdg-open $(pwd) &>/dev/null &<CR><CR>", desc = "Open file explorer", icon = "󱢴" },
-  { "<leader>i", icon = "", group = "Paste images" },
+  { "<leader>i", icon = "", group = "Paste images" },
   { "<leader>ii", ':r!imaging.sh . <CR><CR>', desc = "Find and paste image paths", },
   { "<leader>is", ':r!imaging.sh "${SCREENSHOT_DIR:-$HOME/Pictures}" <CR><CR>', desc = "Screenshot images", },
   { "<leader>ip", ':r!imaging.sh "${HOME}/Pictures/SM-S921W/DCIM"   <CR><CR>', desc = "Phone photos", },
