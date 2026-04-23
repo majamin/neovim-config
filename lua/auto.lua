42ab83e8d35a73fc1f1df03ab04567b5a0442ad1
Marian Minar <majamin@gmail.com>
2026-04-22 21:09:12 -0700

    highlight when yanking

--- lua/auto.lua
+++ lua/auto.lua
@@ -1,3 +1,8 @@
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+  callback = function() vim.hl.on_yank() end,
+})
+
 vim.api.nvim_create_autocmd("FileType", {
   pattern = { "qf" },
   callback = function()
